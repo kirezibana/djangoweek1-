0be36565750f5801bfd4a2bbcd5bed20e8548763
kirezibana <52407699+kirezibana@users.noreply.github.com>
2021-04-12 22:23:17 +0200

    Add files via upload

---
+++ photo/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.fashion,name = 'fashion'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'location/(\d+)',views.filter_by_location,name='location')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
